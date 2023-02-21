b4cea93ed439f55443e90d8e4102884a087cde55
ldlaigo <ldlaigo@student.apc.edu.ph>
2023-02-21 18:11:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,6 +49,12 @@ li a {
 li a:hover {
   background-color: #111;
 }
+body {
+  background-color: rgba(225,197,100, 0.8);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
 </style>
 <body>
 <div class="topnav">
@@ -59,12 +65,6 @@ li a:hover {
   <a href="#about">About</a>
 </div>
 <div class = "divbody">
-<p id="date"></p>
-<script>
-  let d = new Date();
-  document.getElementById("date").innerHTML = "Today's date is"  + d;
-</script>
-<img src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.6435-9/65507555_380824879228187_2085077021569843200_n.jpg?stp=c0.23.206.206a_dst-jpg_p206x206&_nc_cat=111&ccb=1-7&_nc_sid=da31f3&_nc_eui2=AeFY7kFDXIbJlQSxEuiGM_dJexdoI8q8uMR7F2gjyry4xMfBfklmN1Tiy-W70Jpew2b3Pg17NC2g01BeN7rftdwR&_nc_ohc=VWuMcEULuKgAX8YEHeP&_nc_ht=scontent.fmnl17-2.fna&oh=00_AfB76V8-bP1CdEY-8-kOP7P2O-IY5yUT5oK3Atqw7h8iMw&oe=63EC8E4E" alt="My picture" style="display: block; margin-left: auto; margin-right: auto; width:25%;height:25%;">
 <h1 style="text-align:center;"> HELLO!! I AM YURI! WELCOME TO MY PERSONAL BLOG </h1>
 <p style="text-align:center;">I'm 19, I study comsci in APC since grade 11 with a 50% scholarship fee, I chose this course mainly because of the high demanding job which hopefully will lead me to my goal of having fanancial stability</p>
 <p style="text-align:center;"> If it wasn't clear enough my hobbies are playing games and watching films of any kind, but mainly anime </p>
@@ -72,10 +72,10 @@ li a:hover {
 
 <a href="anime.vc/watch/bocchi-the-rock-17479?ep=95894">Watch Bocchi</a>
 <img src="https://media.tenor.com/vuF1B4DJMEIAAAAM/bocchi-the-rock-nijika.gif" alt="Crying Nijika">
-<p style="background-color:tomato;">Style Lesson</p>
+<p>Style Lesson</p>
 <h2><b>TRUST ME!</b></h2>
 <a href="https://matias.ma/nsfw/">https://matias.ma/nsfw/</a>
-<p style="background-color:tomato;">THIS IS A OBLIGATORY NIJIKA</p>
+<p>THIS IS A OBLIGATORY NIJIKA</p>
 <img src="https://cdn.myanimelist.net/images/characters/16/491305.jpg" alt="Obligatory Nijika">
 <p><q><i>I m-miss my sister</i></q> - Nijika 2022 </p>
 </div>

91244061b8b95e0cf4204bbbe9df868794680b8b
ldlaigo <ldlaigo@student.apc.edu.ph>
2023-02-21 18:14:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -70,14 +70,14 @@ body {
 <p style="text-align:center;"> If it wasn't clear enough my hobbies are playing games and watching films of any kind, but mainly anime </p>
 <!-- Please Watch Bocchi the Rock -->
 
-<a href="anime.vc/watch/bocchi-the-rock-17479?ep=95894">Watch Bocchi</a>
-<img src="https://media.tenor.com/vuF1B4DJMEIAAAAM/bocchi-the-rock-nijika.gif" alt="Crying Nijika">
-<p>Style Lesson</p>
-<h2><b>TRUST ME!</b></h2>
-<a href="https://matias.ma/nsfw/">https://matias.ma/nsfw/</a>
-<p>THIS IS A OBLIGATORY NIJIKA</p>
-<img src="https://cdn.myanimelist.net/images/characters/16/491305.jpg" alt="Obligatory Nijika">
-<p><q><i>I m-miss my sister</i></q> - Nijika 2022 </p>
+<a style="text-align:center;" href="anime.vc/watch/bocchi-the-rock-17479?ep=95894">Watch Bocchi</a>
+<img style="text-align:center;" src="https://media.tenor.com/vuF1B4DJMEIAAAAM/bocchi-the-rock-nijika.gif" alt="Crying Nijika">
+<p style="text-align:center;">Style Lesson</p>
+<h2 style="text-align:center;"><b>TRUST ME!</b></h2>
+<a style="text-align:center;" href="https://matias.ma/nsfw/">https://matias.ma/nsfw/</a>
+<p style="text-align:center;">THIS IS A OBLIGATORY NIJIKA</p>
+<img style="text-align:center;" src="https://cdn.myanimelist.net/images/characters/16/491305.jpg" alt="Obligatory Nijika">
+<p style="text-align:center;"><q><i>I m-miss my sister</i></q> - Nijika 2022 </p>
 </div>
 </body>
 </html>

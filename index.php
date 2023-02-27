658306451514857ac61ce861595c62baf550a9eb
ldlaigo <119918582+ldlaigo@users.noreply.github.com>
2023-02-27 17:34:51 +0800

    additonal

--- index.php
+++ index.php
@@ -69,7 +69,51 @@ body {
 <p style="text-align:center;">I'm 19, I study comsci in APC since grade 11 with a 50% scholarship fee, I chose this course mainly because of the high demanding job which hopefully will lead me to my goal of having fanancial stability</p>
 <p style="text-align:center;"> If it wasn't clear enough my hobbies are playing games and watching films of any kind, but mainly anime </p>
 <!-- Please Watch Bocchi the Rock -->
-
+<h1 style="text-align:center;"> My Favorite Animes!</h1> 
+<ul class="image-list-small">
+    <li>
+      <a href="#" style="background-image: url('https://pbs.twimg.com/media/FfYmRVbUcAAzwKE?format=jpg&name=large');"></a>
+      <div class="details">
+        <h3><a href="#">Bocchi the Rock!</a></h3>
+        <p class="date">Fall 2022</p>
+      </div>
+    </li>
+    <li>
+      <a href="#" style="background-image: url('https://cdn.vox-cdn.com/thumbor/ccsnR0gnt4qdXvdD3IL1xlhuKsw=/0x0:1920x1080/1200x0/filters:focal(0x0:1920x1080):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/24319935/22719387.jpg');"></a>
+      <div class="details">
+        <h3><a href="#">Chainsaw Man</a></h3>
+        <p class="date">Fall 2022</p>
+      </div>
+    </li>
+    <li>
+      <a href="#" style="background-image: url('https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/10/Mob-Psycho-100-Season-3-1-scaled-e1634624616827.jpg?fit=1811%2C1017&amp;ssl=1');"></a>
+      <div class="details">
+        <h3><a href="#">Mob Psycho 100</a></h3>
+        <p class="date">Fall 2022</p>
+      </div>
+    </li>
+    <li>
+      <a href="#" style="background-image: url('https://resizing.flixster.com/oiRj9p9jfMCGBa3WA0zJ8TJ8Bwk=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvMzBiMzU5YzktOGJjOC00ODVhLTk0Y2MtYmUwZjA1Yjk5YTgwLnBuZw==');"></a>
+      <div class="details">
+        <h3><a href="#">Spy x Family</a></h3>
+        <p class="date">Fall 2022</p>
+      </div>
+    </li>
+    <li>
+      <a href="#" style="background-image: url('https://pbs.twimg.com/media/Fl8DSdLacAUkMJw?format=jpg&name=large');"></a>
+      <div class="details">
+        <h3><a href="#">Gundam: Witch from Mercury</a></h3>
+        <p class="date">Fall 2022</p>
+      </div>
+    </li>
+    <li>
+      <a href="#" style="background-image: url('https://m.media-amazon.com/images/M/MV5BMTRjYjA3NGMtOTY5Mi00ZTRiLWI5YWEtODAxZDU3ZTFmMmIzXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_.jpg');"></a>
+      <div class="details">
+        <h3><a href="#">Akiba Maid War</a></h3>
+        <p class="date">Fall 2022</p>
+      </div>
+    </li>
+  </ul>
 <a style="display: block; margin-left: auto; margin-right: auto;" href="https://anime.vc/watch/bocchi-the-rock-17479?ep=95894"><p style="text-align:center;">Watch Bocchi</p></a>
 <img style="display: block; margin-left: auto; margin-right: auto;" src="https://media.tenor.com/vuF1B4DJMEIAAAAM/bocchi-the-rock-nijika.gif" alt="Crying Nijika">
 <p style="text-align:center;">Style Lesson</p>
@@ -79,51 +123,7 @@ body {
 <img style="display: block; margin-left: auto; margin-right: auto;" src="https://cdn.myanimelist.net/images/characters/16/491305.jpg" alt="Obligatory Nijika">
 <p style="text-align:center;"><q><i>I m-miss my sister</i></q> - Nijika 2022 </p>
 
-    <?php
-$job = "developer";
-echo "My dream job is to become a " . $job . ". <br>";
-print "Nice to meet you! <br>";
-$x = 2003;
-$y = 2023;
-$age = $y - $x;
-echo "Again I am". $age ."this year!"
-if ($age >= 18) {
-  echo "I may be eternally 15 but yes I am an adult. <br>";
-} else {
-  echo "bro no way. <br>";
-}
 
-echo "Don't mind this random number, it's for loop"
-for ($i = 0; $i < 5; $i++) {
-  echo $i . " ";
-}
-echo "this too"
-function square($num) {
-  return $num * $num;
-}
-echo "The square of 5 is " . square(5) . ". <br>";
-echo "and this..."
-$colors = array("red", "not green", "blue");
-echo "My favorite color is " . $colors[1] . ". <br>";
-echo "especially this..."
-$person = array(
-  "name" => "Bocchi",
-  "job" => "Rock"
-);
-echo $person["name"] . " is the " . $person["job"] . ". <br>";
-
-echo "lastly this"
-echo "The current PHP file is " . $_SERVER["PHP_SELF"] . ". <br>";
-echo "The user's IP address is " . $_SERVER["REMOTE_ADDR"] . ". <br>";
-
-echo "OKAY I PROMISE THIS WILL BE THE LAST RANDOM INFORMATION IN THIS WEBSITE I JUST NEED IT FOR REQUIREMENTS OKAY!"
-$phone_number = "0929-817-5456";
-if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone_number)) {
-  echo "Valid phone number. <br>";
-} else {
-  echo "Invalid phone number. <br>";
-}
-?>
 </div>
 </body>
 </html>
